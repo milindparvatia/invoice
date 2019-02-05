ccaf660163c67104dbe8a630c91ff2609eb7d6bf
Milind Parvatia <milind1997@gmail.com>
2019-02-05 15:14:44 +0530

    first model added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+TYPE_CHOICES = [('goods', 'Goods'),
+                ('services', 'Services')]
+
+TAX_CHOICES = [('tax', 'taxable'),
+               ('non', 'nontaxable')]
+
+INFO_CHOICES = [('sales', 'sales'),
+                ('purchase', 'purchase')]
+
+
+class Items(models.Model):
+    type = models.CharField(choices=TYPE_CHOICES, max_length=10)
+    image = models.ImageField(upload_to='media_data')
+    item_name = models.TextField(max_length=50)
+    sku = models.PositiveIntegerField()
+    unit = models.PositiveIntegerField()
+    hsn_sac = models.TextField(max_length=50)
+    tax = models.CharField(choices=TAX_CHOICES, max_length=10)
+    information = models.CharField(choices=INFO_CHOICES, max_length=10)
+    rate = models.FloatField()
+    amount = models.FloatField()
+    description = models.CharField(max_length=100, blank=True, default='')

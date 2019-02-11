f364fc64b6552818996ec062e050d0f3196f1b52
Milind Parvatia <milind1997@gmail.com>
2019-02-11 21:26:14 +0530

    layout created

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('items/', views.items, name='items'),
+    path('purchase/', views.purchase, name='purchase'),
+    path('reports/', views.reports, name='reports'),
+    path('settings/', views.settings, name='settings'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,6 +7,22 @@ def index(request):
     return render(request, 'app/index.html')
 
 
+def purchase(request):
+    return render(request, 'app/purchase.html')
+
+
+def settings(request):
+    return render(request, 'app/settings.html')
+
+
+def reports(request):
+    return render(request, 'app/reports.html')
+
+
+def items(request):
+    return render(request, 'app/items.html')
+
+
 def contact(request):
     if request.method == 'POST':
         form = ItemsForm(request.POST)

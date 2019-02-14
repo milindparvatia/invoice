5e747e485487064a80c9d3162a1c238ae4ef1979
saloni242 <saloni24298@gmail.com>
2019-02-14 14:21:22 +0530

    models made

--- app/models.py
+++ app/models.py
@@ -8,6 +8,31 @@ TAX_CHOICES = [('tax', 'taxable'),
 
 INFO_CHOICES = [('sales', 'sales'),
                 ('purchase', 'purchase')]
+        
+CUST_CHOICES = [('v', 'Vendor'),
+                ('c', 'Client')]
+
+GEN_CHOICES = [('MR', 'Mr.'),
+               ('MRS', 'Mrs.')]
+
+CURR_CHOICES = [('inr', 'INR'),
+                ('usd', 'USD')]
+
+GST_CHOICES = [('A', 'A'),
+               ('B', 'B')]
+
+PAY_CHOICES = [('paytm', 'PAYTM'),
+               ('cod', 'COD')]
+
+COUN_CHOICES = [('India', 'INDIA'),
+                ('USA', 'USA')]
+
+CITY_CHOICES = [('A', 'A'),
+                ('B', 'B')]
+
+
+STATE_CHOICES = [('A', 'A'),
+                 ('B', 'B')]
 
 
 class Items(models.Model):
@@ -26,13 +51,35 @@ class Items(models.Model):
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
-    item_name = models.CharField(max_length=50)
-    sku = models.PositiveIntegerField()
-    unit = models.PositiveIntegerField()
-    hsn_sac = models.CharField(max_length=50)
-    tax = models.CharField(choices=TAX_CHOICES, max_length=10)
-    information = models.CharField(choices=INFO_CHOICES, max_length=10)
-    rate = models.FloatField()
-    amount = models.FloatField()
-    description = models.CharField(max_length=100, blank=True, default='')
+    last_name = models.CharField(max_length=50)
+    customer = models.CharField(choices=CUST_CHOICES, max_length=20)
+    gender = models.CharField(choices=GEN_CHOICES, max_length=20)
+    mobile = models.PositiveIntegerField()
+    work = models.PositiveIntegerField()
+    email = models.EmailField()
+    company = models.CharField(max_length=100)
+    website = models.CharField(max_length=150)
+    skype = models.CharField(max_length=150)
+    dept = models.CharField(max_length=100)
 
+
+class Other_details(models.Model):
+    gst_treatment = models.CharField(choices=GST_CHOICES, max_length=20)
+    tax = models.CharField(choices=TAX_CHOICES, max_length=10)
+    currency = models.CharField(choices=CURR_CHOICES, max_length=20)
+    gst_number = models.PositiveIntegerField()
+    place = models.CharField(max_length=100)
+    payment = models.CharField(choices=PAY_CHOICES, max_length=20)
+
+
+class Address(models.Model):
+    country = models.CharField(choices=COUN_CHOICES, max_length=120)
+    add1 = models.CharField(max_length=100)
+    add2 = models.CharField(max_length=100)
+    city = models.CharField(choices=CITY_CHOICES, max_length=120)
+    state = models.CharField(choices=STATE_CHOICES, max_length=120)
+    pincode = models.PositiveIntegerField()
+
+
+class Notes(models.Model):
+    note = models.TextField(max_length=150, default='Type your Note here...')

eb8a97511bcb080729bf19f9ef7ef79316b6e8dd
saloni242 <saloni24298@gmail.com>
2019-02-14 14:50:30 +0530

    customer form added

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from .models import Items
+from .models import Items, Customer
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -23,6 +23,32 @@ class ItemsForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ItemsForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = [
+            'first_name',
+            'last_name',
+            'customer',
+            'gender',
+            'mobile',
+            'work',
+            'email',
+            'company',
+            'website',
+            'skype',
+            'dept'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class = 'col-lg-2'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Items
-from .forms import ItemsForm
+from .models import Items, Customer
+from .forms import ItemsForm, CustomerForm
 
 
 def index(request):
@@ -36,6 +36,15 @@ def items(request):
     return render(request, 'app/items.html', instance)
     
 
-
 def contact(request):
-    return render(request, 'app/contact.html')
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+    else:
+        form = CustomerForm()
+        print('error')
+    instance = {
+        'form': form
+    }
+    return render(request, 'app/contact.html', instance)

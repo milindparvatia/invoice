ccb4dc97da1741378ecac7fad35b7bba6a5a7310
saloni242 <saloni24298@gmail.com>
2019-02-22 16:55:48 +0530

    changed sidebar again

--- app/views.py
+++ app/views.py
@@ -7,10 +7,6 @@ def index(request):
     return render(request, 'app/index.html')
 
 
-def purchase(request):
-    return render(request, 'app/purchase.html')
-
-
 def settings(request):
     return render(request, 'app/settings.html')
 
@@ -19,21 +15,21 @@ def reports(request):
     return render(request, 'app/reports.html')
 
 
-def items(request):
-    if request.method == 'POST':
-        form = ItemsForm(request.POST)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            sku = form.cleaned_data['sku']
-            instance.save()
-            print(sku)
-    else:
-        form = ItemsForm()
-        print('error')
-    instance = {
-        'form': form
-    }
-    return render(request, 'app/items.html', instance)
+# def items(request):
+#     if request.method == 'POST':
+#         form = ItemsForm(request.POST)
+#         if form.is_valid():
+#             instance = form.save(commit=False)
+#             sku = form.cleaned_data['sku']
+#             instance.save()
+#             print(sku)
+#     else:
+#         form = ItemsForm()
+#         print('error')
+#     instance = {
+#         'form': form
+#     }
+#     return render(request, 'app/items.html', instance)
 
 
 def contact(request):
@@ -96,6 +92,46 @@ def currency(request):
 
 def translocking(request):
     return render(request, 'app/Accountant/translocking.html')
+
+
+def bills(request):
+    return render(request, 'app/purchase/bills.html')
+
+
+def expenses(request):
+    return render(request, 'app/purchase/expenses.html')
+
+
+def paymade(request):
+    return render(request, 'app/purchase/paymade.html')
+
+
+def purchaseorders(request):
+    return render(request, 'app/purchase/purchaseorders.html')
+
+
+def recurringbills(request):
+    return render(request, 'app/purchase/recurringbills.html')
+
+
+def recurringexpenses(request):
+    return render(request, 'app/purchase/recurringexpenses.html')
+
+
+def vendorcredit(request):
+    return render(request, 'app/purchase/vendorcredit.html')
+
+
+def items(request):
+    return render(request, 'app/items/items.html')
+
+
+def inventoryad(request):
+    return render(request, 'app/items/inventoryad.html')
+
+
+def pricelist(request):
+    return render(request, 'app/items/pricelist.html')
 
 
 def banking(request):
